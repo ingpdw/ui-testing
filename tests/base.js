9ffc4327bf8a00e739f557ac7c8404237f058a7c
pdw <you@example.com>
2017-08-08 16:11:12 +0900

    sample

--- tests/base.js
+++ tests/base.js
@@ -1,40 +1,23 @@
-module.exports = {
-  test: function( browser ) {
-    var screenshotsPath = 'screenshots/';
-    var siteUrl = 'http://pdwapi.herokuapp.com/kpop';
+/*
+ * main test js
+ */
 
-    //connect site
-    browser
-      .url( siteUrl )
-      .waitForElementVisible('iframe', 3000);
+var kpop = require( '../task/kpop.js' );
+var editor = require( '../task/editor.js' );
 
-    browser.expect.element( 'iframe' ).to.be.present;
+function getTask(){
+  var task = {};
+  var sites = process.argv[ 3 ];
 
-    browser.expect.element( 'iframe' ).to.have.attribute( 'src' );
-
-    browser.expect.element( 'iframe' ).to.have.attribute( 'src' ).which.matches( /youtube/ );
-
-    browser.expect.element( 'ol li' ).to.be.present;
-
-    browser.expect.element( 'ol li:nth-child(100)' ).to.be.present;
-
-    browser.resizeWindow( 1280, 800 );
-
-    browser.saveScreenshot( screenshotsPath + '/1280.png' );
-
-    browser.resizeWindow( 320, 800 );
-
-    browser.saveScreenshot( screenshotsPath + '/300.png' );
-
-    browser
-      .click( 'ol li:nth-child(1)' )
-      .waitForElementVisible('ol li:nth-child(1) iframe', 3000)
-      .saveScreenshot( screenshotsPath + '/select.png' );
-
-      browser
-        .click( 'ol li:nth-child(1) iframe' )
-        .saveScreenshot( screenshotsPath + '/run.png' );
-
-    browser.end()
+  if( sites == 'kpop' ){
+    Object.assign( task, kpop );
   }
+
+  if( sites == 'editor' ){
+    Object.assign( task, editor );
+  }
+
+  return task;
 }
+
+module.exports = getTask();
